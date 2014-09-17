47866dc40ff9c777263c5ff698a3c58db7c6bec6
Stepan Shamaiev <skipp@ukr.net>
2014-09-17 13:27:40 +0300

    fix static serving

--- dynamic/urls.py
+++ dynamic/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 admin.autodiscover()
 
@@ -8,9 +9,11 @@ urlpatterns = patterns('',
     url(r'^$', 'dynmod.views.home', name='home'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^upload_file/', 'dynmod.views.upload_file', name='upload_file'),
-    url(r'^static/(?P<path>.*)$', 'django.views.static.serve'),
+    # url(r'^static/(?P<path>.*)$', 'django.views.static.serve'),
     url(r'^get/(?P<table>.*)/$', 'dynmod.views.get_table'),
     url(r'^create/(?P<table_name>.*)/$', 'dynmod.views.create'),
     url(r'^post/row/$', 'dynmod.views.post_row'),
     url(r'^delete/row/$', 'dynmod.views.delete_row'),
 )
+
+urlpatterns += staticfiles_urlpatterns()
